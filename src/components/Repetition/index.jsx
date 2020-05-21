aafbe6bad0cf154fc3469f6dc0f6f326add7a0ad
romluc <romulofilhofilho@gmail.com>
2020-05-21 15:06:56 -0300

    Create custom hook useFetch

--- src/components/Repetition/index.jsx
+++ src/components/Repetition/index.jsx
@@ -1,26 +1,41 @@
-import React from 'react';
-
-function getProducts() {
-  fetch('db.json')
-    .then((res) => res.json())
-    .then((data) => console.log(data));
-}
-
-getProducts();
+import React, { useState, useEffect } from 'react';
 
 const Repetition = (props) => {
+  const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+      const fetchData = async () => {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        setData(data);
+        setLoading(false);
+      };
+      fetchData();
+    }, [url]);
+
+    return { data, loading };
+  };
+
+  const { data: products, loading } = useFetch(
+    'http://localhost:3000/products'
+  );
+
   return (
     <div>
       <h2>Repetition</h2>
       <ul>
-        {/* {getProducts().then((data) => {
-          data.map((prod) => (
+        {loading ? (
+          <h3>Loading...</h3>
+        ) : (
+          products.map((prod) => (
             <li key={prod.id}>
               {prod.id} - {prod.name} => CAD
               {prod.price}
             </li>
-          ));
-        })} */}
+          ))
+        )}
       </ul>
     </div>
   );
